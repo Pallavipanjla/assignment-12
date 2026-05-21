c7ae38da19d2e5062c172a0b7988784260008da9
Pallavipanjla <107753203+Pallavipanjla@users.noreply.github.com>
2026-05-21 10:55:50 +0530

    Update studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,21 +1,17 @@
 const express = require("express");
-
 const router = express.Router();
-
 const studentController = require("../controllers/studentController");
 
+// GET /students         - Get all students (supports ?search=)
+// POST /students/add    - Add new student
+// GET /students/:id     - Get single student
+// POST /students/update/:id - Update student
+// GET /students/delete/:id  - Delete student
+
 router.get("/", studentController.getAllStudents);
-
-router.get("/add", studentController.addStudentForm);
-
 router.post("/add", studentController.addStudent);
-
-router.get("/view/:id", studentController.viewStudent);
-
-router.get("/edit/:id", studentController.editStudentForm);
-
+router.get("/:id", studentController.viewStudent);
 router.post("/update/:id", studentController.updateStudent);
-
 router.get("/delete/:id", studentController.deleteStudent);
 
 module.exports = router;
